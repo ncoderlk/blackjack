d32271ff7e15a0d209f6636aa8c5331ff8a00f42
ncoderlk <ncoderlk@gmail.com>
2022-05-26 15:57:55 +0530

    Updated

--- js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  const person = {
-    cr: "Current Score",
-    img_src:"../images/other/head.png"
-  };
-  document.getElementById("head").src = person.img_src;
-  document.getElementById("curnscore").innerHTML = person.cr;
-  document.getElementById('restart').style.display="none";
-})
---
+++ js/game.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("reset").style.display = "none";
+  const app = {
+    generateAnother: "Generate Another Card::?",
+    won: "You Have Won This Game...",
+    lost: "You Have Lost This Game...",
+    error: "404 Brain Not Found...Try Reload This Page...",
+    reload: "Please reset The Game...!",
+    //Generate
+  };
+  const $ = document.querySelector(".generate");
+  $.addEventListener("click", () => {
+    var $$$ = document.getElementById("instruction").innerHTML;
+    if ($$$ == app.lost) {
+      alert(app.lost + app.reload);
+      document.getElementById("reset").style.display = "block";
+    } else if ($$$ == app.won) {
+      document.getElementById("reset").style.display = "block";
+      alert(app.won);
+    } else {
+      //Random Number
+      var randomNumber;
+      do {
+        randomNumber = Math.floor((Math.random() * 100) / 7.6);
+      } while (randomNumber == 0);
+      //Card Image Source
+      var image = document.createElement("img");
+      var imageSource = "./images/" + randomNumber + ".png";
+      image.src = imageSource;
+      document.getElementById("content").appendChild(image);
+
+      //Score
+      var subScore = parseInt(document.getElementById("score").innerHTML);
+      subScore += randomNumber;
+      document.getElementById("score").innerHTML = subScore;
+      //Win Lost Manager
+      if (subScore < 21) {
+        document.getElementById("instruction").innerHTML = app.generateAnother;
+      } else if (subScore == 21) {
+        document.getElementById("instruction").innerHTML = app.won;
+      } else if (subScore > 21) {
+        document.getElementById("instruction").innerHTML = app.lost;
+      } else {
+        document.getElementById("instruction").innerHTML = app.error;
+      }
+    }
+  });
+  //Restart button
+  const btn$$ = document.querySelector(".rest");
+  btn$$.addEventListener("click", () => {
+    document.getElementById("instruction").innerHTML = "";
+    location.reload();
+  });
+});
